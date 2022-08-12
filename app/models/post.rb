2e19ccb99b4f7c664ef01c006a82b3113f9a00e0
Chase <chasedemaster.tech@gmail.com>
2022-08-12 10:09:53 -0500

    commit tree 174c469ffdcfcf4c6e910586711770779f1333e6

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+    
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    # validates :category, inclusion: { in: %w(Fiction or Non-Fiction) }
+    validate :believe
+   
+    def believe 
+        if title.downcase === "fiction" | "non-fiction"
+            errors.add(:title, "You Won't Believe These True Facts")
+        end
+    end
+    # validates :title, exclusion: { in: %w() }
+    # validates :title, inclusion: { in: %w(True Facts) }
+
+
+
 end
